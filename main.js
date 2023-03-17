dcbc45693ee3e4ff7d8bd52aa97cd587f9db03a9
Philipp <philipp@geuer-pollmann.de>
2023-03-17 09:13:42 +0100

    schreibfehler entfernt und Gitignore hinzugefügt

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 });
 
+function setDarkMode(){
+    document.cookie = "theme=dark; expires=Fri, 31 Dec 2023 23:59:59 UTC";
+}
+
+function setLightMode(){
+    document.cookie = "theme=light; expires=Fri, 31 Dec 2023 23:59:59 UTC";
+}
+
+function getThemeFromCookie(){
+    var cookie = document.cookie.split(";")
+    cookie = cookie[i].trim();
+    for(var i = 0; i < cookie.length; i++){
+        if (cookie.indexOf("theme")){
+            var theme = cookie.substring("theme".length, cookie.length);
+            return theme;
+        }
+    }
+}
+
+function ShowCookiePopup(){
+    print("Hello World");
+}
+
+ShowCookiePopup();
