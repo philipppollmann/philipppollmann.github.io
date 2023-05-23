e65f2a3fe84d9afe8a297eab067d412f2013278d
Philipp <philipp@geuer-pollmann.de>
2023-05-23 13:01:13 +0200

    changes im Header

--- scripts/WorkExperience.js
+++ scripts/WorkExperience.js
@@ -1,5 +1,14 @@
-const circles = document.querySelectorAll(".circle")
-progressbar = document.querySelector(".indicator")
-buttons = document.querySelectorAll("button")
+const WorkExperienceStep = document.querySelectorAll('.WorkExperienceStep'),
+    progressBar = document.querySelector(".indicator"),
+    buttons = document.querySelectorAll("button");
 
-let currentStep = 1
+let currentStep = 1;
+
+const updateSteps = (event) =>{
+    currentStep = event.target.id === "next" ? ++currentStep: --currentStep;
+    console.log(currentStep);
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", updateSteps);
+})
