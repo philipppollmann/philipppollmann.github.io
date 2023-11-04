050e426ee79ea11ebc01f8d439a427f97676440d
Philipp <philipp@geuer-pollmann.de>
2023-11-04 15:07:16 +0100

    pipeline angepast

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+
 export default defineNuxtConfig({
     app: {
         baseURL: '/',
         buildAssetsDir: 'assets'
     },
     devtools: { enabled: true },
-      components: true,
-      css:  ['@/assets/css/tailwind.css'],
+    components: true,
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxtjs/color-mode'
